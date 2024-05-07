6592f02e29b35650f0a971b624552fb90d307b56
sebousan <sebousan@gmail.com>
2024-05-07 12:13:03 +0200

    remove dropdown

--- assets/js/vendors/bootstrap.js
+++ assets/js/vendors/bootstrap.js
@@ -1,7 +1,6 @@
 // Import the Bootstrap components we want to use.
 // See https://github.com/twbs/bootstrap/blob/main/js/index.umd.js
 
-import Dropdown from 'js/bootstrap/src/dropdown';
 import Offcanvas from 'js/bootstrap/src/offcanvas';
 
-export { Dropdown, Offcanvas };
+export { Offcanvas };
